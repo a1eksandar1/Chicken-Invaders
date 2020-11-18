1d613f000727f896e8eb8b48c1763d399ed0c052
crnomarkovicm <majacrnomarkovic@live.com>
2020-11-18 12:20:36 +0100

    Dodati dugmici na pocetni ekran

--- MATF_Chicken_Invaders/src/main.cpp
+++ MATF_Chicken_Invaders/src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.showFullScreen();
-    //w.show();
+    w.setWindowState(Qt::WindowFullScreen);
+    w.show();
     return a.exec();
 }
