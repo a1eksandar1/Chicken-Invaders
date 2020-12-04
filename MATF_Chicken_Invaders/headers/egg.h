3cbce415981e7d21cfc454e34ee57dccb39b83ac
crnomarkovicm <majacrnomarkovic@live.com>
2020-12-04 15:14:11 +0100

    Izmene u klasi Egg
    Former-commit-id: 7b0bcd30dc6997e736600114d59f1667b54478f8

--- MATF_Chicken_Invaders/headers/egg.h
+++ MATF_Chicken_Invaders/headers/egg.h
@@ -10,11 +10,13 @@ class Egg: public QObject, public QGraphicsPixmapItem
 
 public:
     Egg();
+
 public slots:
     void move();
     void clean();
 
-
+private:
+    int width, height;
 };
 
 #endif // EGG_H
--- MATF_Chicken_Invaders/src/egg.cpp
+++ MATF_Chicken_Invaders/src/egg.cpp
@@ -2,6 +2,8 @@
 #include <QTimer>
 #include <QGraphicsScene>
 #include <QList>
+#include <QScreen>
+#include <QApplication>
 
 Egg::Egg()
 {
@@ -11,12 +13,20 @@ Egg::Egg()
     connect(moveTimer, SIGNAL(timeout()), this, SLOT(move()));
     moveTimer->start(50);
 
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QRect  screenGeometry = screen->geometry();
+    int height = screenGeometry.height();
+    int width = screenGeometry.width();
+
+    this->width = width;
+    this->height = height;
+
 }
 
 void Egg::move()
 {
     setPos(x(), y()+10);
-    if(pos().y() > 700)
+    if(pos().y() > height - 150)
     {
         setPixmap(QPixmap(":images/chicken/egg_2.png").scaled(50,50,Qt::KeepAspectRatio));
         QTimer *cleanTimer = new QTimer(this);
